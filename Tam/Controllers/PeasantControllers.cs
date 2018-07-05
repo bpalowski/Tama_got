d4d499739fc3318a9e7d3815447ab3372990298d
Brian Palowski, James Hanley and William Kinzig <james@>
2018-07-05 16:39:33 -0700

    Not Done

--- Tam/Controllers/PeasantControllers.cs
+++ Tam/Controllers/PeasantControllers.cs
@@ -6,13 +6,34 @@ namespace Tamagotchi.Controllers
 {
     public class PeasantController : Controller
     {
-        [HttpGet("/Peasants")]
+        [HttpGet("/peasants")]
         public ActionResult Index()
         {
-
+          List<Peasant> allPeasants = Peasant.GetAll();
+          return View(allPeasants);
         }
 
-        
+        [HttpGet("/peasants/new")]
+        public ActionResult CreateForm()
+        {
+          return View();
+        }
+
+        [HttpPost("/peasants/")]
+        public ActionResult Create()
+        {
+          Peasant newPeasant = new Peasant();
+          List<Peasant> allPeasants = Peasant.GetAll();
+          return View("Index", allPeasants);
+        }
+        [HttpGet("/peasants/{id}")]
+        public ActionResult Details(int id)
+        {
+          Peasant peasant = Peasant.Find(id);
+          return View(peasant);
+        }
+
+
 
 
     }
--- Tam/Models/Peasant.cs
+++ Tam/Models/Peasant.cs
@@ -10,57 +10,49 @@ namespace Tamagotchi.Models
         private int _attention;
         private int _sleep;
         private int _health;
+        private int _idCounter = 0;
 
         private static List<Peasant> _instances = new List<Peasant> {};
 
-        public Peasant()
+        public Peasant(int health = 100)
         {
-          _instances.Add(this);
-          _id = _instances.Count;
+          ++_idCounter;
+          _id = _idCounter;
           _food = 100;
           _attention = 100;
           _sleep = 100;
-          _health = 100;    
+          _health = health;
+          _instances.Add(this);
         }
 
-        public void GetId()
+        public int GetId()
         {
           return _id;
         }
-        public void GetFood()
+        public int GetFood()
         {
           return _food;
         }
-        public void GetAttention()
+        public int GetAttention()
         {
           return _attention;
         }
-        public void GetSleep()
+        public int GetSleep()
         {
           return _sleep;
         }
-        public void GetHealth()
+        public int GetHealth()
         {
           return _health;
         }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        public static List<Peasant> GetAll()
+        {
+          return _instances;
+        }
+        public static Peasant Find(int searchID)
+        {
+            return _instances[searchID];
+        }
 
     }
 }

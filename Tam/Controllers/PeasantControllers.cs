5e39bd04188d48e416761474cc09927fe9836295
Brian Palowski, James Hanley and William Kinzig <william@>
2018-07-05 11:58:46 -0700

    add constructor and get methods

---
+++ Tam/Controllers/PeasantControllers.cs
@@ -0,0 +1,19 @@
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Tamagotchi.Models;
+
+namespace Tamagotchi.Controllers
+{
+    public class PeasantController : Controller
+    {
+        [HttpGet("/Peasants")]
+        public ActionResult Index()
+        {
+
+        }
+
+        
+
+
+    }
+}
--- Tam/Models/Peasant.cs
+++ Tam/Models/Peasant.cs
@@ -5,6 +5,62 @@ namespace Tamagotchi.Models
 {
     public class Peasant
     {
+        private int _id;
+        private int _food;
+        private int _attention;
+        private int _sleep;
+        private int _health;
+
+        private static List<Peasant> _instances = new List<Peasant> {};
+
+        public Peasant()
+        {
+          _instances.Add(this);
+          _id = _instances.Count;
+          _food = 100;
+          _attention = 100;
+          _sleep = 100;
+          _health = 100;    
+        }
+
+        public void GetId()
+        {
+          return _id;
+        }
+        public void GetFood()
+        {
+          return _food;
+        }
+        public void GetAttention()
+        {
+          return _attention;
+        }
+        public void GetSleep()
+        {
+          return _sleep;
+        }
+        public void GetHealth()
+        {
+          return _health;
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     }
 }
